1bd592da5a418ba090d209ed3cf9d12a9caa62b0
Venom-1369 <gauravmane2201@gmail.com>
2024-09-15 13:23:40 +0530

    Utils code implementation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 import express from "express";
-
+import cors from "cors"
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    Credential : true
+}))
+
+app.use(express.json({limit : "15kb"}))//which helps to avoid server crash from json limit
+app.use(express.urlencoded({extended:true,limit:"15kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 export {app};
