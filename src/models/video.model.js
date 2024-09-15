ff909a56825a7f21a37e5e3b40d9adb96113ff5e
Venom-1369 <gauravmane2201@gmail.com>
2024-09-15 14:42:08 +0530

    add model and implement jwt tokens

---
+++ src/models/video.model.js
@@ -0,0 +1,57 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile : {
+            tyep : String,
+            required : true
+        },
+
+        thumbnail : {
+            type : String,
+            required : true
+        },
+
+        title : {
+            type : String,
+            required : true
+        },
+
+        description : {
+            type : String,
+            required : true
+        },
+
+        duration : {
+            type : Number,
+            required : true
+        },
+
+        views : {
+            type : Number,
+            default : 0
+        },
+
+        isPublished : {
+            type : Boolean,
+            default : true
+        },
+
+        owner : {
+            type : Schema.Types.ObjectId,
+            ref : "User"
+        },
+
+
+    },
+    {
+        Timestamp : true
+    }
+)
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+
+export const Video = mongoose.model("Video",videoSchema)
